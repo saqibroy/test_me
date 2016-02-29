2f910785079e8b3851181286b07d97d42dd18f29
saqibroy <saqibsohail007@gmail.com>
2016-02-29 14:38:47 +0500

    done

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,7 +2,7 @@ Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
-  config.serve_static_assets = true
+  config.action_controller.asset_host = "//#{ENV['FOG_DIRECTORY']}.s3.amazonaws.com"
   config.cache_classes = true
 
   # Eager load code on boot. This eager loads most of Rails and
@@ -35,6 +35,8 @@ Rails.application.configure do
   # Asset digests allow you to set far-future HTTP expiration dates on all assets,
   # yet still be able to expire them through the digest params.
   config.assets.digest = true
+  config.assets.enabled = true
+  config.assets.initialize_on_precompile = true
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
