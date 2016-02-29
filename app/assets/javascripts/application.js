792a672c3ed0f6ccd33e99752302b685f1774394
saqibroy <saqibsohail007@gmail.com>
2016-02-29 11:24:47 +0500

    deploy

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,36 @@
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
+// or any plugin's vendor/assets/javascripts directory can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// compiled file.
+//
+// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
+// about supported directives.
+//
+//= require jquery
+//= require jquery_ujs
+//= require js/plugins/jquery/jquery-ui.min
+//= require bootstrap
+//= require bootstrap-sprockets
+//= require turbolinks
+//= require js/plugins/icheck/icheck.min
+//= require js/plugins/mcustomscrollbar/jquery.mCustomScrollbar.min
+//= require js/plugins/scrolltotop/scrolltopcontrol
+//= require js/plugins/morris/raphael-min
+//= require js/plugins/morris/morris.min
+//= require js/plugins/rickshaw/d3.v3
+//= require js/plugins/rickshaw/rickshaw.min
+//= require js/plugins/jvectormap/jquery-jvectormap-1.2.2.min
+//= require js/plugins/jvectormap/jquery-jvectormap-world-mill-en
+//= require js/plugins/bootstrap/bootstrap-datepicker
+//= require js/plugins/owl/owl.carousel.min
+//= require js/plugins/moment.min
+//= require js/settings
+//= require js/plugins
+//= require js/actions
+//= require js/demo_dashboard
+
+
